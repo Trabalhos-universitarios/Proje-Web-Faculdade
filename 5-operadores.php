45679db05c224fb0da6dd58fda06c2d92e331361
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2020-11-07 12:55:05 -0300

    atualização 07/11

---
+++ 5-operadores.php
@@ -0,0 +1,26 @@
+<?php
+
+echo '<p><h3>Operadores</h3></p>';
+echo '<p>
+        Um operador é um elemento que recebe o valor de uma ou mais expressões, e que retorna
+        um outro valor. Vejamos a seguir quais são os operadores do PHP.
+
+        <li><b>Operadores aritméticos: </b>servem para efetuar operações de adição(+), subtração(-), 
+        multiplicação(*), divisão(/), módulo(%) e exponencial(**).</li>
+
+        <li><b>Operadores de atribuição: </b> servem para atgribuir o valor da expressão à direita para o esperando
+        da esquerda(=).</li>
+
+        <li><b>Operadores bit a bit(bitwise): </b>servem para avaliar e modificar bits específicos em um tipo de dados
+        inteiro(AND, OR, XOR, NOT, <<"e">>), a letra "e" é sem aspas.</li>
+
+        <li><b>Operadores de comparação: </b>servem para coparar valores iguais (==), idênticos (===), diferentes(!=ou<>),
+        não idênticos(!==), menor que (<), maior que(>), menor ou igual (>=). Seu retorno é TRUE ou FALSE.3</li>
+
+        <li><b>Operadores de controle: </b>o sinal de arroba @ serve, quando preceder uma expressão em PHP, para ignorar qualquer mensagem de erro
+        gerada pela sua execução.</li>
+
+        <li><b>Operadores de incremento e decremento: </b></li>
+      </p>';
+
+?>
