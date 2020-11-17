113d261111334fc58370c33ab6fb1868f4704318
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2020-11-17 11:46:22 -0300

    atualizando pagina principal

--- scripts/frontEnd/php/paginaPrincipal.php
+++ scripts/frontEnd/php/paginaPrincipal.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="stylePagPrincipal.css">
+    <link rel="stylesheet" href="../css/stylePagPrincipal.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Borrow Bag</title>
@@ -14,7 +14,7 @@
     </div>
     <div id="menuOpcoes">
         <ul>
-            <li><a href="bagagensTamanhoGG.html" title="GG">Tamanho GG</a></li>
+            <li><a href="../html/bagagensTamanhoGG.html" title="GG">Tamanho GG</a></li>
             <li><a href="bagagensTamanhoG.html" title="G">Tamanho G</a></li>
             <li><a href="bagagensTamanhoM.html" title="M">Tamanho M</a></li>
             <li><a href="bagagensTamanhoP.html" title="P">Tamanho P</a></li>
@@ -25,15 +25,15 @@
         <br><h1>Seja bem vindo ao Borrow Bag!</h1></br>
     </div>
     <section class="slide">
-        <img class="imgSlide" src="img/Captura de Tela (0).png">>
-        <img class="imgSlide" src="img/Captura de Tela (02).png">
-        <img class="imgSlide" src="img/Captura de Tela (03).png">
-        <img class="imgSlide" src="img/Captura de Tela (04).png">
-        <img class="imgSlide" src="img/Captura de Tela (05).png">
-        <img class="imgSlide" src="img/Captura de Tela (06).png">
-        <img class="imgSlide" src="img/Captura de Tela (07).png">
-        <img class="imgSlide" src="img/Captura de Tela (08).png">
-        <img class="imgSlide" src="img/Captura de Tela (09).png">
+        <img class="imgSlide" src="../img/Captura de Tela (0).png">>
+        <img class="imgSlide" src="../img/Captura de Tela (02).png">
+        <img class="imgSlide" src="../img/Captura de Tela (03).png">
+        <img class="imgSlide" src="../img/Captura de Tela (04).png">
+        <img class="imgSlide" src="../img/Captura de Tela (05).png">
+        <img class="imgSlide" src="../img/Captura de Tela (06).png">
+        <img class="imgSlide" src="../img/Captura de Tela (07).png">
+        <img class="imgSlide" src="../img/Captura de Tela (08).png">
+        <img class="imgSlide" src="../img/Captura de Tela (09).png">
     </section>
 
     <section id="rodape">
