02000535a4beea7596d9984c3a8525b5ef4082a5
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2020-11-17 00:08:52 -0300

    finalizando front end da  tela login e cadastro

--- cadastro.php
+++ cadastro.php
@@ -2,100 +2,45 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/styleCadastro.css">
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Pagina interna</title>
 </head>
 
-<body>
-    <div id="cadastro">
-        <form method="POST" action="">
-            <!--Quando o Back End estiver pronto a ação será cadastrar nele os dados do visitante-->
-            <h1>Cadastro</h1>
+<!-- Esse trecho de código é responsável por atacar o body da tela de cadastro -->
+<style>
+    body {
+        background-image: url("../img/img-login-02.jpg");
+        position: relative;
+        height: 109vh;
+        width: 40vw;
+        background-size: cover;
+        color: orangered;
+        text-align: center;
+    }
+</style>
 
-            <h4>Dados pessoais:</h4>
+<body>
+    <h1>Cadastro</h1>
+    <form method="POST" action="processaDados.php">
+        <!--Aqui está os inputs do formulário de cadastro-->
+        <div id="corpo-form-cad">
+            <input id="primeiro_nome" name="primeiro_nome" require="required" type="text" placeholder="Digite seu primeito nome" />
+            <input id="ultimo_nome" name="ultimo_nome" require="required" type="text" placeholder="Digite seu último nome" />
+            <input id="email" name="email" require="required" type="email" placeholder="Digite seu e-mail" />
+            <input id="numero_celular" name="numero_celular" require="required" type="tel" pattern="{2} \d{5} \d{4}" placeholder="Ex: 41 99999 9999" />
+            <input id="nome_usuario" name="nome_usuario" required="required" type="text" placeholder="Escolha um nome de usuário" />
+            <input id="senha" name="senha" required="required" type="password" placeholder="Escolha uma senha" />
+            <input id="confere_senha" name="confere_senha" require="required" type="password" placeholder="Repita a senha" />
+            <input type="submit" value="cadastrar" />
+            <strong>OU</strong>
             <p>
-                <label for="primeiro_nome_cadastro">Primeiro nome</label>
-                <input id="primeiro_nome_cadastro" name="primeiro_nome_cadastro" required="required" type="text"
-                    placeholder="Digite seu primeiro nome...">
+                <a href="login.php"><strong>Voltar ao login</strong></a> <!-- essa ancora permite voltar ao login -->
             </p>
-            <p>
-                <label for="ultimo_nome_cadastro">Último nome</label>
-                <input id="ultimo_nome_cadastro" name="ultimo_nome_cadastro" required="required" type="text"
-                    placeholder="Digite seu último nome...">
-            </p>
-            <p>
-                <label for="email_cadastro">E-mail</label>
-                <input id="email_cadastro" name="email_cadastro" required="required" type="email"
-                    placeholder="Digite seu e-mail...">
-            </p>
-            <p>
-                <label for="telefone_cadastro">Telefone com DDD</label>
-                <input id="telefone_cadastro" name="telefone_cadastro" required="required" type="tel"
-                    pattern="\d{2} \d{5}-\d{4}" placeholder="Ex: 41 99999-9999">
-            </p>
-            <p>
-                <h4>Endereço:</h4>
-                <label for="cep_cadastro">CEP</label>
-                <input id="cep_cadastro" name="cep_cadastro" required="required" type="number"
-                    placeholder="Digite seu CEP">
-            </p>
-            <p>
-                <label for="logradouro_cadastro">Logradouro</label>
-                <input id="logradouro_cadastro" name="logradouro_cadastro" required="required" type="text"
-                    placeholder="Ex: Rua, Av Etc...">
-            </p>
-            <p>
-                <label for="complemento_cadastro">Complemento</label>
-                <input id="complemento_cadastro" name="complemento_cadastro" required="required" type="text"
-                    placeholder="Ex: casa, apartamento, Etc...">
-            </p>
-            <p>
-                <label for="numero_casa_cadastro">Número</label>
-                <input id="numero_casa_cadastro" name="numero_casa_cadastro" required="required" type="number" placeholder="Digite o número da residência">
-            </p>
-            <p>
-                <label for="bairro_cadastro">Bairro</label>
-                <input id="bairro_cadastro" name="bairro_cadastro" required="required" type="text"
-                    placeholder="Digite o bairro onde reside...">
-            </p>
-            <p>
-                <label for="cidade_cadastro">Cidade</label>
-                <input id="cidade_cadastro" name="cidade_cadastro" required="required" type="text"
-                    placeholder="Digite a cidade onde mora">
-            </p>
-            <p>
-                <label for="estado_cadastro">Estado</label>
-                <input id="estado_cadastro" name="estado_cadastro" required="required" type="text"
-                    placeholder="Digite o estado onde mora">
-            </p>
-            <p>
-                <label for="pais_cadastro">País</label>
-                <input id="pais_cadastro" name="pais_cadastro" required="required" type="text"
-                    placeholder="Digite o país onde mora">
-            </p>
-            <p>
-                <h4>Criar Senha:</h4>
-                <form action="confereSenha.php" method="POST">
-                    <label for="senha_cadastro">Senha</label>
-                    <input id="senha_cadastro" name="senha_cadastro" required="required" type="password"
-                        placeholder="Digite a senha">
-            </p>
-            <p>
-                <label for="confirma_senha_cadastro">Confirmar Senha</label>
-                <input id="confirma_senha_cadastro" name="confirma_senha_cadastro" required="required" type="password"
-                    placeholder="Confirmar senha">
-            </p>
-            <p>
-                <input type="submit" value="cadastrar"/>
-            </p>
-            OU
-            <p>
-                <a href="login.php">Voltar ao login</a>
-            </p>
-        </form>
+    </form>
     </div>
+
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -3,17 +3,31 @@
 
 <head>
     <title>Login</title>
-    <link rel="stylesheet" href="css/styleLogin.css">
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
+<!-- Esse trecho de código é responsável por atacar o body da tela de login -->
+<style>
+    body{
+    background-image: url("../img/img-login-02.jpg");
+    position: relative;
+    height: 80vh;
+    width: 80vw;
+    background-size: cover;
+    color: orangered;
+    text-align: center;
+}
+
+</style>
+
 <body>
-    <!--Aqui ficará as tags do formulário de login-->
+    <!--Aqui está os inputs do formulário de login-->
     <div id="corpo-form">
         <h1>Login</h1>
         <form method="POST" action="processaDados.php">
-            <!--Quando o Back End estiver pronto a ação será buscar nele o login e a senha-->
+            <!--Quando o Back End estiver pronto a ação será buscar nele o login e a senha, ou seja, no documento processaDados.php-->
             <input id="email_usuario" name="nome_usuario" required="required" type="email" placeholder="Usuário" />
             <input id="senha" name="senha" required="required" type="password" placeholder="Senha" />
             <input id="submit" type="submit" name="logar" value="logar" />
