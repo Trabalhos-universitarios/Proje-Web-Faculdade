77f4c365b5c29b18cda48c579c9455c78d7f8002
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2020-11-20 16:05:04 -0300

    finalizando css paginas de itens para emorestimo

--- scripts/frontEnd/bagagens php/bagagensTamanhoGG.php
+++ scripts/frontEnd/bagagens php/bagagensTamanhoGG.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="../css/styleBagagens.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -15,29 +16,47 @@
     </style>
 </head>
 <body>
+    <h1>Lista de bagagens tamanho GG</h1>
+    <form method="$_POST">
     <div id="imagens">
-        <a href="#">
-            <br><img src="..\img\Captura de Tela (01).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (02).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (03).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (04).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (05).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (06).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (07).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (08).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (09).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (10).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (11).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (12).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (13).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-            <br><img src="..\img\Captura de Tela (14).png" width="500px"><a href="#">Verificar disponibilidade</a><br>
-        </a><br>
+            <br><img src="..\img\Captura de Tela (01).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (02).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (03).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (04).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (05).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (06).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (07).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (08).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (09).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (10).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (11).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (12).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (13).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+            <br><img src="..\img\Captura de Tela (14).png" width="500px">
+            <input id="submit" name="verificar" type="submit" value="Verificar disponibilidade" />
+        <br>
   
     </div>
+    </form>
+    <form method="$_POST" action="../php/paginaPrincipal.php">
     <div id="voltar">
         <br>
-        <a href="../php/paginaPrincipal.php" style="font-size: 30px; position: absolute;">voltar</a>
+        <input id="submit" name="voltar" type="submit" value="Voltar"></input>
     </div>
+    </form>
     
 </body>
 </html>
